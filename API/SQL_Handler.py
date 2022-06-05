55da1c74280fa43b4e02223744af3c9cbfffca0f
lugashiot <77395933+lugashiot@users.noreply.github.com>
2022-06-05 13:26:03 +0200

    get methodes are working

--- API/SQL_Handler.py
+++ API/SQL_Handler.py
@@ -5,15 +5,15 @@ import sys
 class DBHandler():
     def __init__(self) -> None:
         try:
-            conn = mariadb.connect(
+            self.conn = mariadb.connect(
                 user="feedback",
                 password="susadmin1234",
                 host="localhost",
                 port=3306,
                 database="School"
             )
-            conn.autocommit = True
-            self.cur = conn.cursor()
+            self.conn.autocommit = True
+            self.cur = self.conn.cursor()
         
         except mariadb.Error as e:
             print(f"Error connecting to MariaDB Platform: {e}")
@@ -30,7 +30,7 @@ class DBHandler():
     def get_class_mails(self, school_class : str) -> list:
         mail_list = []
         #sql Select Email FROM Classes_Emails WHERE School_Class = 'school_class'
-        self.cur.execute("Select Email FROM `Classes_Emails` WHERE School_Class = '?';", (school_class))
+        self.cur.execute("Select Email FROM `Classes_Emails` WHERE School_Class = ?;", (school_class, ))
         for out in self.cur:
             mail_list.append(str(out[0]))
         return mail_list
@@ -39,37 +39,45 @@ class DBHandler():
 # UUIDs
     def write_UUID(self, uuid : str, teacher_id : int, school_class : str, uuid_used = False):
         #sql INSERT INTO `UUIDs`(UUID, Teacher_ID, School_Class, UUID_Used) VALUES (uuid, teacher_id, school_class, uuid_used)
-        self.cur.execute("INSERT INTO `UUIDs` (UUID, Teacher_ID, School_Class, UUID_Used) VALUES (?, ?, ?, ?);", (uuid, teacher_id, school_class, uuid_used))
+        self.cur.execute("INSERT INTO `UUIDs` (UUID, Teacher_ID, School_Class, UUID_Used, Answer_1, Answer_2, Answer_3, Answer_4) VALUES (?, ?, ?, ?, '0', '0', '0', '0');", (uuid, teacher_id, school_class, uuid_used))
         return
 
-    def write_Answers(self, uuid : str, answer_1 : int, answer_2 : int, answer_3 : int, answer_4 : int):
+    def write_Answers(self, uuid : str, answer_1 : str, answer_2 : str, answer_3 : str, answer_4 : str):
         #sql UPDATE `UUIDs` SET (UUID_Used = True, `Answer_1` = answer_1, `Answer_2` = answer_2, `Answer_3` = answer_3, `Answer_4` = answer_4)
         self.cur.execute("UPDATE `UUIDs` SET (UUID_Used = True, `Answer_1` = ?, `Answer_2` = ?, `Answer_3` = ?, `Answer_4` = ?) WHERE UUID = '?';", (answer_1, answer_2, answer_3, answer_4, uuid))
         return
     
     def is_UUID_Used(self, uuid : str):
         #sql SELECT UUID_Used FROM `UUIDs` WHERE UUID = uuid
-        self.cur.execute("SELECT UUID_Used FROM `UUIDs` WHERE UUID = '?';", (uuid))
+        self.cur.execute("SELECT UUID_Used FROM `UUIDs` WHERE UUID = ?;", (uuid, ))
         for out in self.cur:
             return out[0]
 
-    def get_Answers_by_uuid(self, uuid : str):
+    def get_answers_by_uuid(self, uuid : str) -> list:
         #sql SELECT Answer_1,Answer_2,Answer_3,Answer_4 FROM `UUIDs` WHERE UUID = uuid
-        self.cur.execute("SELECT Answer_1,Answer_2,Answer_3,Answer_4 FROM `UUIDs` WHERE UUID = '?';", (uuid))
+        self.cur.execute("SELECT Answer_1,Answer_2,Answer_3,Answer_4 FROM `UUIDs` WHERE UUID = ?;", (uuid, ))
         answer_list = []
         for out in self.cur:
             for answer in out:
                 answer_list.append(int(answer))
+            # if an unvalid/unfilled answer is in the answerset, give back an empty one
+            if '0' in answer_list:
+                return []
             return answer_list
 
-    def get_Answers_for_class(self, teacher_id : int, school_class : str):
+    def get_answers_for_class(self, teacher_id : int, school_class : str):
         #sql Select Answer_1,Answer_2,Answer_3,Answer_4 FROM `UUIDs` WHERE (Teacher_ID = teacher_id AND School_Class = school_class)
-        self.cur.execute("SELECT Answer_1,Answer_2,Answer_3,Answer_4 FROM `UUIDs` WHERE (Teacher_ID = ? AND School_Class = '?');", (teacher_id, school_class))
+        self.cur.execute("SELECT Answer_1,Answer_2,Answer_3,Answer_4 FROM `UUIDs` WHERE (Teacher_ID = ? AND School_Class = ?);", (teacher_id, school_class))
         answer_list_list = []
+        # iterate through every answerset given
         for out in self.cur:
             answer_list = []
+            # iterate through every answer of the current answerset
             for answer in out:
                 answer_list.append(int(answer))
+            # if any answer in the answerset is 0, dont use it
+            if '0' in answer_list:
+                continue
             answer_list_list.append(answer_list)
         return answer_list_list
 
@@ -82,7 +90,7 @@ class DBHandler():
 
     def get_class_assignments(self, teacher_id : int ) -> list:
         #sql Select School_Class FROM Teachers_Classes WHERE Teacher_ID = teacher_id
-        self.cur.execute("Select School_Class FROM `Teachers_Classes` WHERE Teacher_ID = '?';", (teacher_id))
+        self.cur.execute("Select School_Class FROM `Teachers_Classes` WHERE Teacher_ID = ?;", (teacher_id, ))
         class_list = []
         for out in self.cur:
             class_list.append(str(out[0]))
@@ -97,27 +105,39 @@ class DBHandler():
 
     def get_teacher_by_id(self, teacher_id, wanted_key = ""):
         #sql Select * FROM `Teachers` WHERE Teacher_ID = teacher_id
-        self.cur.execute("Select * FROM `Teachers` WHERE Teacher_ID = '?';", (teacher_id))
+        self.cur.execute("Select * FROM `Teachers` WHERE Teacher_ID = ?;", (teacher_id, ))
         for out in self.cur:
-            self.teacher_formatter(wanted_key)
-            return 
+            return self.teacher_formatter(out, wanted_key)
 
     def get_teacher_by_username(self, username, wanted_key = ""):
         #sql Select * FROM `Teachers` WHERE Username = username      
-        self.cur.execute("Select * FROM `Teachers` WHERE Username = '?';", (username))
+        self.cur.execute("Select * FROM `Teachers` WHERE Username = ?;", (username, ))
         for out in self.cur:
-            return self.teacher_formatter(self.cur, wanted_key)
+            return self.teacher_formatter(out, wanted_key)
     
-    def teacher_formatter(self, cursor, wanted_key):
-        teacher = {}
-        for out in cursor:
-            teacher["Teacher_ID"] = out[0]
-            teacher["Username"] = out[1]
-            teacher["Forename"] = out[2]
-            teacher["Lastname"] = out[3]
-            teacher["Email"] = out[4]
+    def teacher_formatter(self, out, wanted_key):
+        teacher = {
+            "Teacher_ID" : out[0],
+            "Username" : out[1],
+            "Forename" : out[2],
+            "Lastname" : out[3],
+            "Email" : out[4]
+        }
 
         for key in teacher.keys():
             if key == wanted_key:
                 return teacher[key]
         return teacher
+
+
+if __name__ == "__main__":
+    Handle = DBHandler()
+    print("handled")
+
+    print(Handle.get_class_mails("4CHEL"))
+    x = Handle.get_teacher_by_id(8)
+    print(x)
+    print(x["Forename"])
+    print(Handle.get_teacher_by_username("Christian.Hilpold", "Email"))
+    print(Handle.get_class_assignments(8))
+    #print(Handle.get_answers_for_class(1, "4CHEL"))
