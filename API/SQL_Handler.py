8acf5528e8960f41c4ecb0acbd5cd16e5f0f1e68
lugashiot <77395933+lugashiot@users.noreply.github.com>
2022-06-09 01:12:30 +0200

    SQL Handler not ready to use

--- API/SQL_Handler.py
+++ API/SQL_Handler.py
@@ -1,3 +1,4 @@
+import re
 import mariadb
 import sys
 
@@ -106,18 +107,42 @@ class DBHandler:
 
 
 # Teachers_Classes
-    def write_teacher_class_assignment(self, teacher_id : int, school_class : str):
-        #sql INSERT INTO `Teachers_Classes` (Assignment_ID, Teacher_ID, School_Class) VALUES (NULL, teacher_id, school_class)
-        self.cur.execute("INSERT INTO `Teachers_Classes` (Assignment_ID, Teacher_ID, School_Class) VALUES (NULL, ?, ?);", (teacher_id, school_class))
+    def write_teacher_class_assignment(self, teacher_id : int, school_class : str, subject : str):
+        #sql INSERT INTO `Teachers_Classes` (Assignment_ID, Teacher_ID, School_Class, Subject) VALUES (NULL, teacher_id, school_class, Subject)
+        self.cur.execute("INSERT INTO `Teachers_Classes` (Assignment_ID, Teacher_ID, School_Class, Subject) VALUES (NULL, ?, ?, ?);", (teacher_id, school_class, subject))
         return "write_teacher_class_assignment done"
 
-    def get_class_assignments(self, teacher_id : int ) -> list:
-        #sql Select School_Class FROM Teachers_Classes WHERE Teacher_ID = teacher_id
-        self.cur.execute("Select School_Class FROM `Teachers_Classes` WHERE Teacher_ID = ?;", (teacher_id, ))
+    def add_questions_to_assignment(self, teacher_id : str, school_class : str, subject : str, question_id_1 : int, question_id_2 : int, question_id_3 : int, question_id_4 : int, assignment_id = 0):
+        if assignment_id == 0:
+            assignment_id = self.get_assignment_id(teacher_id, school_class, subject)
+        #sql Update `Teachers_Classes` SET Question_1 = question_id_1, Question_2 = question_id_2, Question_3 = question_id_3, Question_4 = question_id_4 WHERE Assignment_ID = assignment_id
+        self.cur.execute("Update `Teachers_Classes` SET Question_1 = ?, Question_2 = ?, Question_3 = ?, Question_4 = ? WHERE Assignment_ID = ?", (question_id_1, question_id_2, question_id_3, question_id_4, assignment_id))
+        return "add_questions_to_assignment done"
+        
+    def get_class_assignments(self, teacher_id : int) -> list:
+        #sql Select School_Class FROM `Teachers_Classes` WHERE Teacher_ID = teacher_id
+        self.cur.execute("Select School_Class,Subject FROM `Teachers_Classes` WHERE Teacher_ID = ?;", (teacher_id, ))
         class_list = []
         for out in self.cur:
-            class_list.append(str(out[0]))
+            class_list.append(str(out[0]) + " - " + str(out[1]))
         return class_list
+
+    def get_assignment_id(self, teacher_id : str, school_class : str, subject : str) -> int:
+        #sql Select Assignment_ID FROM `Teachers_Classes` WHERE Teacher_ID = teacher_id AND School_Class = school_class AND Subject = subject
+        self.cur.execute("Select Assignment_ID FROM `Teachers_Classes` WHERE Teacher_ID = ? AND School_Class = ? AND Subject = ?", (teacher_id, school_class, subject))
+        for out in self.cur:
+            return int(out[0])
+    
+    def get_question_ids(self, teacher_id : str, school_class : str, subject : str, assignment_id = 0) -> list: #assignment_id OVERRIDES other parameters
+        if assignment_id == 0:
+            assignment_id = self.get_assignment_id(teacher_id, school_class, subject)
+        #sql Select Question_1,Question_2,Question_3,Question_4 FROM `Teachers_Classes` WHERE Assignment_ID = assignment_id
+        self.cur.execute("Select Question_1,Question_2,Question_3,Question_4 FROM `Teachers_Classes` WHERE Assignment_ID = ?;", (assignment_id, ))
+        question_list = []
+        for out in self.cur:
+            for elem in out:
+                question_list.append(elem)
+            return question_list
 
 
 # Teachers
@@ -160,6 +185,14 @@ class DBHandler:
                 return teacher[key]
         return teacher
 
+
+# Questions
+    def write_question(self, teacher_id : int, question_text : str, ans_opt_1 : str, ans_opt_2 : str, ans_opt_3 : str, ans_opt_4 : str, ans_opt_5 : str, is_public = 0):
+        #sql INSERT INTO `Questions` (Question_ID, Teacher_ID, Is_Public, Question_Text, Answer_Option_1, Answer_Option_2, Answer_Option_3, Answer_Option_4, Answer_Option_5) VALUES (NULL, teacher_id, is_public, question_text, ans_opt_1, ans_opt_2, ans_opt_3, ans_opt_4, ans_opt_5)
+        self.cur.execute("INSERT INTO `Questions` (Question_ID, Teacher_ID, Is_Public, Question_Text, Answer_Option_1, Answer_Option_2, Answer_Option_3, Answer_Option_4, Answer_Option_5) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)", (teacher_id, is_public, question_text, ans_opt_1, ans_opt_2, ans_opt_3, ans_opt_4, ans_opt_5))
+        return "write_question done"
+
+# SQL HANDLER NOT READY TO USE
 
 if __name__ == "__main__":
     db = DBHandler()
