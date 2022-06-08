2997403203f980ea0dff55e342a8fa8908ff2cb0
BuildTools <unconfigured@null.spigotmc.org>
2022-06-08 22:12:05 +0200

    clean up

--- API/feedback/database.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import authenticate
-
-
-user = authenticate(username='Stecher.Helmut', password='HTLbesteSchual123!')
-if user is not None:
-    print("Allah")
-else:
-    print("Bruh")
