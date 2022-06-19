13e288a43a83e8447a58d65d890bed03611616d1
BuildTools <unconfigured@null.spigotmc.org>
2022-06-20 01:48:52 +0200

    Frontend upgrades

--- API/teachers/views.py
+++ API/teachers/views.py
@@ -198,7 +198,8 @@ def create_poll(request):
                     card.selected_flag = False
                     return return_()
                 elif request.POST[str(card.btn_name)] == "Löschen":
-                    db.Questions.delete_teacher_from_question(teacher.teacher_id)
+                    db.Questions.delete_teacher_from_question(card.btn_name)
+                    cards.question_cards.pop(cards.question_cards.index(card))
                     return return_()
             else:
                 return return_(error_msg="Auswahl nicht erkannt!")
