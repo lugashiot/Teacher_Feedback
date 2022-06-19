e5905529cf4bc06803fca185d30d1637755f6e9e
lugashiot <77395933+lugashiot@users.noreply.github.com>
2022-06-19 23:02:08 +0200

    Default question fix

--- API/teachers/views.py
+++ API/teachers/views.py
@@ -180,7 +180,7 @@ def create_poll(request):
                         return return_(success_msg=f"Umfrage erfolgreich an {', '.join(requested_assignments)} gesendet")
                     return return_(error_msg="Fehler beim Senden der Mails aufgetreten")
 
-            elif [x for x in teacher.questions if str(x.question_id) in request.POST]:
+            elif [x for x in (teacher.questions + [Question(id) for id in [1,2,3,4]]) if str(x.question_id) in request.POST]:
                 card = [x for x in cards.question_cards if str(x.btn_name) in request.POST][0]
                 if request.POST[str(card.btn_name)] == "Hinzufügen":
                     if len([x for x in cards.question_cards if x.selected_flag is True]) < 6:
